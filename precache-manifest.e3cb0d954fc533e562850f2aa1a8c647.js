2d1b470aeb9fdd1a5ee3c3d70493bb9611c5d161
Your Name <you@example.com>
2019-12-05 14:08:17 +0200

    pdf sample added

---
+++ precache-manifest.e3cb0d954fc533e562850f2aa1a8c647.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00bc10537b22ab46cca0234213e93b8b",
+    "url": "/reportsapp/index.html"
+  },
+  {
+    "revision": "5ca4686aaa24074d62fd",
+    "url": "/reportsapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "96281fe64cf119386be8",
+    "url": "/reportsapp/static/js/2.8eb9b2ea.chunk.js"
+  },
+  {
+    "revision": "5ca4686aaa24074d62fd",
+    "url": "/reportsapp/static/js/main.6c953399.chunk.js"
+  },
+  {
+    "revision": "97939caa4876a82b7517",
+    "url": "/reportsapp/static/js/runtime-main.bbf7ecf6.js"
+  },
+  {
+    "revision": "9a6fb6f5cd3aa4ab1adaaab1f693f266",
+    "url": "/reportsapp/static/media/Rubik-Bold.9a6fb6f5.ttf"
+  },
+  {
+    "revision": "b3d0902b533ff4c4f1698a2f96ddabab",
+    "url": "/reportsapp/static/media/Rubik-Regular.b3d0902b.ttf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reportsapp/static/media/logo.25bf045c.svg"
+  }
+]);
